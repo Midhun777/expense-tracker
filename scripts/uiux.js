9179fef546ece35da431cb1d7b0ec3f75223ee30
Midhun Mathew <midmat4828@gmail.com>
2025-01-05 14:30:00 +0530

    TEST

--- scripts/uiux.js
+++ scripts/uiux.js
@@ -4,7 +4,6 @@ const oweNav = document.getElementById("oweNav");
 const lendNav = document.getElementById("lendNav");
 const secTitle = document.getElementById("secTitle");
 
-//
 const expCont = document.getElementById("exp-cont");
 const oweCont = document.getElementById("owe-cont");
 const lendCont = document.getElementById("lend-cont");
