29a83c21217922617807584235d93367f78e6b90
Midhun Mathew <midmat4828@gmail.com>
2025-01-24 18:34:32 +0530

    UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         bgCol: '#fef7f8',
         priRed: '#df3952',
         secGreen: '#9fec88',
+        secGreenDark:'#7cc467',
         accGreen: '#50e38f',
       },
     },
