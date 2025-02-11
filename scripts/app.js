8f9825f8b24611332aa3d45e7f6341f7beb2805e
Midhun Mathew <midmat4828@gmail.com>
2025-02-11 16:19:16 +0530

    db update

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,11 @@
+
+const db = {
+    expenseArr: [],
+    owesArr: [],
+    lendsArr: []
+}; 
+
+
 var expDataLs;
 
 function loadExpenses() {
@@ -7,12 +15,10 @@ function loadExpenses() {
     console.log(JSON.parse(expDataLs));
     var expDataJson = JSON.parse(expDataLs);
 
-
     // let expArr = db.expenseArr;
     expDataJson.forEach(exp => {
         expCont.innerHTML += `
-<div
-                        class="exp-card mt-4 w-full lg:w-[32%] bg-priRed h-[4.7em] lg:h-20 border-2 border-black rounded-lg shadow-md flex mb-1">
+                    <div class="exp-card mt-4 w-full lg:w-[32%] bg-priRed h-[4.7em] lg:h-20 border-2 border-black rounded-lg shadow-md flex mb-1">
                         <div class="card-left w-9/12">
                             <p id="expCardTitle"
                                 class="text-xl font-semibold ml-3 text-textColLight mt-2 lg:mt-2">${exp.name}</p>
@@ -67,6 +73,7 @@ submitExp.addEventListener("click", function () {
 
     console.log(expObj);
     db.expenseArr.push(expObj);
+    console.log(db.expenseArr);
     localStorage.setItem("expenses", JSON.stringify(db.expenseArr));
     // console.log(db.expenseArr);
     console.log("Expense added");
@@ -124,9 +131,67 @@ submitOwe.addEventListener("click", function () {
 
     console.log(oweObj);
     db.owesArr.push(oweObj);
-    localStorage.setItem("expenses", JSON.stringify(db.owesArr));
+    localStorage.setItem("owes", JSON.stringify(db.owesArr));
     // console.log(db.expenseArr);
-    console.log("Expense added");
+    console.log("Owe added");
+    popupCont.classList.add("hidden");
+    popupCont.classList.remove("flex");
+    hidePopup();
+    // clearForm();
+    // }
+
+    function clearForm() {
+        oweName.value = "";
+        oweAmt.value = "";
+        oweDate.value = "";
+        oweCat.value = "";
+        oweDesc.value = "";
+    }
+});
+
+
+
+submitLend.addEventListener("click", function () {
+    let lendName = document.getElementById("title");
+    let lendAmt = document.getElementById("price");
+    let lendDate = document.getElementById("date");
+    let lendCat = document.getElementById("category");
+    let lendDesc = document.getElementById("note");
+
+    // console.log("Expense submitted");
+    // if (expName.value === "" || expAmt.value === "" || expDate.value === "" || expCat.value === "" || expDesc.value === "") {
+    // alert("Please fill all the fields");
+    // } else {
+    // console.log(expName.value, expAmt.value, expDate.value, expCat.value, expDesc.value);
+
+    let lendObj = {
+        name: lendName.value,
+        amount: Number(lendAmt.value),
+        date: lendDate.value,
+        category: lendCat.value,
+        description: lendDesc.value
+    };
+
+    lendCont.innerHTML += `<div
+                        class="exp-card mt-4 w-full lg:w-[32%] bg-priRed h-[4.7em] lg:h-20 border-2 border-black rounded-lg shadow-md flex mb-1">
+                        <div class="card-left w-9/12">
+                            <p id="expCardTitle"
+                                class="text-xl font-semibold ml-3 text-textColLight mt-2 lg:mt-2">${lendName.value}</p>
+                            <p id="expCardDesc"
+                                class="lg:text-sm text-sm ml-3 text-textColLight mt-2 lg:mt-2 font-medium mb-[-10px]">${lendDesc.value} <span> : </span><span id="expCardDate"
+                                    class="date text-[1em] lg:inline md:hidden">
+                                    ${lendDate.value}</span></p>
+                        </div>
+                        <div
+                            class="card-right w-3/12 flex justify-center items-center text-2xl lg:text-3xl font-medium text-textColLight">₹${lendAmt.value}</div>
+                    </div>`;
+
+
+    console.log(lendObj);
+    db.owesArr.push(lendObj);
+    localStorage.setItem("lends", JSON.stringify(db.lendsArr));
+    // console.log(db.expenseArr);
+    console.log("Owe added");
     popupCont.classList.add("hidden");
     popupCont.classList.remove("flex");
     hidePopup();
